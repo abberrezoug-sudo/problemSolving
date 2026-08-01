b5fdaf71475ce4aa2b714fce0ae0f2085d907d6e
abdellah berrezoug <ab.berrezoug@esi-sba.dz>
2026-08-01 23:49:39 +0100

    addLIst

---
+++ listProblem.py
@@ -0,0 +1,11 @@
+# 
+marksStudent = [12,14,16,17]
+sum = 0
+for mark in marksStudent:
+    sum +=mark
+    countSize = len(marksStudent)
+average = sum / len(marksStudent)
+print(f"sum of the mark is {sum}")
+print(f"the average of the marks is {average}")
+print(f"the size of the list is {countSize}")
+
