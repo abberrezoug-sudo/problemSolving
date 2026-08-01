83c2c08af03b477569cfbcf9affbbf884478c4c7
abdellah berrezoug <ab.berrezoug@esi-sba.dz>
2026-08-01 23:23:02 +0100

    count ans sum programe

---
+++ solvingPrograme.py
@@ -0,0 +1,9 @@
+inputNumber = int(input("Enter a number: "))
+summ = 0
+while not inputNumber == 0:
+    summ += inputNumber
+    countNumber += 1
+    inputNumber = int(input("Enter another number (0 to exit): "))
+print(f"the summe of the numbers is {summ}")
+print(f"the count of the number is {countNumber}")
+    
